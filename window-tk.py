434b0f70f82608e3174c27c9fbad7e12719c8dec
TupiNUMBooR <TupiNUMBooR@gmail.com>
2022-06-01 07:57:42 +0300

    Add image to tk

--- window-tk.py
+++ window-tk.py
@@ -2,9 +2,21 @@
 import text
 from tkinter import *
 
+
+def on_button():
+    # https://www.pythontutorial.net/tkinter/tkinter-photoimage/
+    # https://placekitten.com/640/240
+    tk.im = PhotoImage(file="./240.png")
+    l_im.config(image=tk.im)
+    l_txt.config(text=text.get())
+
+
 tk = Tk()
 tk.title("window-tk.py")
-label = Label(tk, text="empty")
-label.pack()
-Button(tk, text="Button", command=lambda: label.config(text=text.get())).pack()
+l_txt = Label(tk, text="empty")
+l_txt.pack()
+l_im = Label(tk)
+l_im.pack()
+Button(tk, text="Button", command=on_button).pack()
+
 tk.mainloop()
