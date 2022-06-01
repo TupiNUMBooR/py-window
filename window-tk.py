3faedefc7ca0e188afe9760c2ccab5535cb304c5
TupiNUMBooR <TupiNUMBooR@gmail.com>
2022-06-01 04:37:01 +0300

    Update tk

--- window-tk.py
+++ window-tk.py
@@ -2,16 +2,9 @@
 import text
 from tkinter import *
 
-
-def on_button():
-    label.config(text=text.get())
-
-
 tk = Tk()
 tk.title("window-tk.py")
-
 label = Label(tk, text="empty")
-label.pack(side=TOP)
-widget = Button(tk, text="Button", command=on_button)
-widget.pack(side=BOTTOM)
+label.pack()
+Button(tk, text="Button", command=lambda: label.config(text=text.get())).pack()
 tk.mainloop()
