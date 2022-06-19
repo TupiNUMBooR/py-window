71722d938562798ad83790aa5dee178f80873781
TupiNUMBooR <TupiNUMBooR@gmail.com>
2022-06-19 08:21:34 +0300

    Make full gtk

--- window-gtk.py
+++ window-gtk.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import text
 import gi
 
 gi.require_version("Gtk", "3.0")
@@ -8,13 +9,23 @@ from gi.repository import Gtk
 class MyWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="Hello World")
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
-        self.button = Gtk.Button(label="Click Here")
+        self.button = Gtk.Button(label="Button")
         self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
+
+        self.label = Gtk.Label(label="123")
+
+        self.image = Gtk.Image()
+
+        self.box.pack_start(self.button, True, True, 0)
+        self.box.pack_start(self.label, True, True, 0)
+        self.box.pack_start(self.image, True, True, 0)
+        self.add(self.box)
 
     def on_button_clicked(self, widget):
-        print("Hello World")
+        self.label.set_text(text.get())
+        self.image.set_from_file('240.avif')
 
 
 win = MyWindow()
